5e4f9353fe3a854d989325c124db4780df52976c
Hartmut Frenzel <hfrenzel@uw.edu>
2021-02-21 15:28:26 -0800

    Testing simple bar chart

--- docs/co2_by_fuel.697fafdf.js
+++ docs/co2_by_fuel.697fafdf.js
@@ -117,12 +117,12 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"ZQwg":[function(require,module,exports) {
-module.exports = "https://cse412.github.io/american-industrial-energy/sunshine.45a98b78.csv";
-},{}],"uEQA":[function(require,module,exports) {
+})({"CApY":[function(require,module,exports) {
+module.exports = "https://cse412.github.io/american-industrial-energy/co2_by_fuel.c06c301c.csv";
+},{}],"NQYw":[function(require,module,exports) {
 "use strict";
 
-var _sunshine = _interopRequireDefault(require("../static/sunshine.csv"));
+var _co2_by_fuel = _interopRequireDefault(require("../static/co2_by_fuel.csv"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -131,82 +131,44 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 // With strict mode, you can not, for example, use undeclared variables
 
 
-var line_svg; // used for svg later
+var co2ByFuelArray = []; // used to store data later
 
-var colorSet; // used for color scheme later
+var options = {
+  config: {// Vega-Lite default configuration
+  },
+  init: function init(view) {
+    // initialize tooltip handler
+    view.tooltip(new vegaTooltip.Handler().call);
+  },
+  view: {
+    // view constructor options
+    // remove the loader if you don't want to default to vega-datasets!
+    //   loader: vega.loader({
+    //     baseURL: "",
+    //   }),
+    renderer: "canvas"
+  }
+};
+vl.register(vega, vegaLite, options); // Again, We use d3.csv() to process data
 
-var sunshineArray = []; // used to store data later
-// preparation for our svg
-
-var margin = {
-  top: 50,
-  right: 35,
-  bottom: 50,
-  left: 50
-},
-    w = 650 - (margin.left + margin.right),
-    h = 520 - (margin.top + margin.bottom);
-var legendSpace = 130;
-console.log(margin); // preparation for our x/y axis
-
-var y = d3.scaleLinear().range([h, 0]);
-var x = d3.scaleTime().range([0, w]);
-var yAxis = d3.axisLeft(y);
-var xAxis = d3.axisBottom(x).tickFormat(d3.timeFormat("%b")); // %b: abbreviated Month format (Mon, Jun..)
-
-var citySet = []; // once finish processing data, make a graph!
-
-d3.csv(_sunshine.default).then(function (data) {
+d3.csv(_co2_by_fuel.default).then(function (data) {
   data.forEach(function (d) {
-    sunshineArray.push(d);
-
-    if (!citySet.includes(d.city)) {
-      citySet.push(d.city);
-    }
+    co2ByFuelArray.push(d);
   });
-  drawLineD3();
+  drawBarVegaLite();
 });
 
-function drawLineD3() {
-  colorSet = d3.scaleOrdinal().domain(citySet).range(d3.schemeSet2);
-  x.domain(d3.extent(sunshineArray, function (d) {
-    return d3.timeParse("%b")(d.month);
-  }));
-  y.domain(d3.extent(sunshineArray, function (d) {
-    return parseFloat(d.sunshine);
-  })); // create our svg
-
-  line_svg = d3.select('#d3-demo').append('svg').attr("id", "line-chart").attr("width", w + margin.left + margin.right + legendSpace).attr("height", h + margin.top + margin.bottom).append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')'); // append x axis to svg
-
-  line_svg.append("g").attr("transform", "translate(0," + h + ")").attr("class", "myXaxis").call(xAxis); // append y axis to svg
-
-  line_svg.append("g").attr("class", "myYaxis").call(yAxis); // create a group to store lines for our line chart
-
-  var path = line_svg.append('g').attr("id", "paths-group");
-  var line = d3.line().x(function (d) {
-    return x(d3.timeParse("%b")(d.month));
-  }).y(function (d) {
-    return y(parseFloat(d.sunshine));
-  }); // make a line for each city
-
-  citySet.forEach(function (d) {
-    var currentCity = sunshineArray.filter(function (e) {
-      return e.city === d;
-    });
-    path.append("path").datum(currentCity).attr("class", "lines").attr('d', line).style("stroke-width", 2.5).style("fill", "none").attr("stroke", colorSet(d));
-  }); // add legend
-
-  var legend = line_svg.append('g').attr("id", "legend-group");
-  legend.selectAll("rect").data(citySet).join("rect").attr("class", "legends").attr("x", 600).attr("y", function (d) {
-    return 25 + 30 * citySet.indexOf(d);
-  }).attr("width", 10).attr("height", 10).style("fill", function (d) {
-    return colorSet(d);
+function drawBarVegaLite() {
+  // var sunshine = add_data(vl, sunshine.csv, format_type = NULL);
+  // your visualization goes here
+  vl.markBar({
+    filled: true,
+    color: 'black'
+  }).data(co2ByFuelArray).encode(vl.x().fieldN('Fuel').sort('none'), vl.y().fieldQ('CO2'), vl.tooltip(['CO2'])).width(450).height(450).render().then(function (viewElement) {
+    // render returns a promise to a DOM element containing the chart
+    // viewElement.value contains the Vega View object instance
+    document.getElementById('co2_by_fuel').appendChild(viewElement);
   });
-  legend.selectAll("text").data(citySet).join("text").attr("class", "legends").attr("x", 620).attr("y", function (d) {
-    return 30 + 30 * citySet.indexOf(d);
-  }).text(function (d) {
-    return d;
-  }).style("font-size", "15px").attr("alignment-baseline", "middle");
 }
-},{"../static/sunshine.csv":"ZQwg"}]},{},["uEQA"], null)
-//# sourceMappingURL=https://cse412.github.io/american-industrial-energy/d3Demo.c3cad310.js.map
+},{"../static/co2_by_fuel.csv":"CApY"}]},{},["NQYw"], null)
+//# sourceMappingURL=https://cse412.github.io/american-industrial-energy/co2_by_fuel.697fafdf.js.map
---
+++ src/co2_by_fuel.js
@@ -0,0 +1,55 @@
+import co2ByFuelData from '../static/co2_by_fuel.csv'    // import dataset
+"use strict";     // the code should be executed in "strict mode".
+                  // With strict mode, you can not, for example, use undeclared variables
+
+var co2ByFuelArray = [];   // used to store data later
+
+const options = {
+  config: {
+    // Vega-Lite default configuration
+  },
+  init: (view) => {
+    // initialize tooltip handler
+    view.tooltip(new vegaTooltip.Handler().call);
+  },
+  view: {
+    // view constructor options
+    // remove the loader if you don't want to default to vega-datasets!
+    //   loader: vega.loader({
+    //     baseURL: "",
+    //   }),
+    renderer: "canvas",
+  },
+};
+
+vl.register(vega, vegaLite, options);
+
+// Again, We use d3.csv() to process data
+d3.csv(co2ByFuelData).then(function(data) {
+  data.forEach(function(d){
+    co2ByFuelArray.push(d);
+  })
+  drawBarVegaLite();
+});
+
+
+function drawBarVegaLite() {
+  // var sunshine = add_data(vl, sunshine.csv, format_type = NULL);
+  // your visualization goes here
+  vl.markBar({filled:true, color:'black'})
+  .data(co2ByFuelArray)
+  .encode(
+      vl.x().fieldN('Fuel').sort('none'),
+      vl.y().fieldQ('CO2'),
+      vl.tooltip(['CO2']),
+  )
+  .width(450)
+  .height(450)
+  .render()
+  .then(viewElement => {
+    // render returns a promise to a DOM element containing the chart
+    // viewElement.value contains the Vega View object instance
+    document.getElementById('co2_by_fuel').appendChild(viewElement);
+  });
+}
+  
